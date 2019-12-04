9b80fa44e6710ca3e30f190a0aa12a3aff05f786
Juan Restrepo <jrestrepo@sangoma.com>
2019-12-03 21:27:15 -0500

    day 2.2

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -1,16 +1,13 @@
 import io.vavr.collection.Array;
 import io.vavr.collection.List;
+import io.vavr.control.Option;
 import utils.FileUtils;
 
 import java.util.Arrays;
 
 public class Day2 {
 
-    public Integer[] runProgram(List<String> fileLines) {
-        String[] codesArray = fileLines.get(0).split(",");
-
-        Integer[] program = Array.of(codesArray).map(Integer::new).toJavaArray(Integer[]::new);
-
+    public Integer[] runProgram(Integer[] program) {
         for (int i = 0; i < program.length; i+=4) {
             System.out.println(String.format("indexes: %s %s %s %s", i, i+1, i+2, i+3));
             Integer opcode = program[i];
@@ -19,6 +16,7 @@ public class Day2 {
                 System.out.println(String.format("Adding %s plus %s and storing %s in index %s", program[program[i+1]], program[program[i+2]], program[program[i+1]] + program[program[i+2]], program[program[i+3]]));
                 program[program[i+3]] = program[program[i+1]] + program[program[i+2]];
             } else if (opcode == 2) {
+                System.out.println(String.format("Multiplying %s times %s and storing %s in index %s", program[program[i+1]], program[program[i+2]], program[program[i+1]] + program[program[i+2]], program[program[i+3]]));
                 program[program[i+3]] = program[program[i+1]] * program[program[i+2]];
             }
         }
@@ -26,9 +24,27 @@ public class Day2 {
         return program;
     }
 
+    public Option<Integer> controlIterations(Array<Integer> program2) {
+        for(int noun = 0; noun < 100; noun++) {
+            for(int verb = 0; verb < 100; verb++) {
+                Integer[] program = program2.toJavaArray(Integer[]::new);
+                program[1] = noun;
+                program[2] = verb;
+                System.out.println(String.format("using program %s", Arrays.asList(program)));
+                Integer[] output = new Day2().runProgram(program);
+                if (output[0] == 19690720) {
+                    return Option.of(100*noun + verb);
+                }
+            }
+        }
+        return Option.none();
+    }
+
     public static void main(String args[]) {
         List<String> fileLines = FileUtils.readFile("src/main/resources/day2.txt");
-        Integer[] output = new Day2().runProgram(fileLines);
-        System.out.println(Arrays.asList(output));
+        String[] codesArray = fileLines.get(0).split(",");
+        Array<Integer> program = Array.of(codesArray).map(Integer::new);
+        Option<Integer> result = new Day2().controlIterations(program);
+        System.out.println(result.get());
     }
 }
--- src/test/java/Day2Test.java
+++ src/test/java/Day2Test.java
@@ -1,3 +1,4 @@
+import io.vavr.collection.Array;
 import io.vavr.collection.List;
 import org.junit.Test;
 
@@ -9,9 +10,11 @@ public class Day2Test {
     @Test
     public void test1() {
         List<String> input = List.of("1,9,10,3,2,3,11,0,99,30,40,50");
+        String[] codesArray = input.get(0).split(",");
+        Integer[] program = Array.of(codesArray).map(Integer::new).toJavaArray(Integer[]::new);
         Day2 day2 = new Day2();
 
-        Integer[] output = day2.runProgram(input);
+        Integer[] output = day2.runProgram(program);
 
         assertThat(output[0], is(3500));
     }
