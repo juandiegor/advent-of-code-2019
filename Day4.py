d611861a443a21bcb389ddae762e0ebf9c6ef8f4
Juan Diego Restrepo <juandiegor@gmail.com>
2019-12-06 10:11:17 -0500

    day4.1

--- Day4.py
+++ Day4.py
@@ -1,22 +1,12 @@
 
-def at_least_two_exact_consecutive(number):
+def at_least_two_consecutives(number):
     number = str(number)
     pos = 0
     for digit in number:
-        if pos < len(number) - 2 and number[pos+1] == digit and number[pos+2] != digit:
-            return True, number
+        if pos != len(number) - 1 and number[pos+1] == digit:
+            return True
         pos += 1
-    return False, None
-
-
-def no_more_than_two_consecutive(number, consec):
-    number = str(number)
-    pos = 0
-    for digit in number:
-        if pos < len(number) - 2 and number[pos+1] == digit and number[pos+2] == digit:
-            return False
-        pos += 1
-    return True
+    return False
 
 
 def all_numbers_same_or_increasing(number):
@@ -36,10 +26,9 @@ def size_6(number):
 def find_passwords(min_n, max_n):
     count = 0
     for i in range(min_n, max_n):
-        at_most_two, _ = at_least_two_exact_consecutive(i)
-        if at_most_two and all_numbers_same_or_increasing(i) and size_6(i):
+        if at_least_two_consecutives(i) and all_numbers_same_or_increasing(i) and size_6(i):
             count += 1
     return count
 
 
-print(find_passwords(111111, 111112))
+print(find_passwords(402328, 864247))
